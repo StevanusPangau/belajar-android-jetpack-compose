9fc3b03966f797ee7332c8fdc4346fd2b30be9a9
Evan <stevanuspangau50@gmail.com>
2024-09-17 22:23:53 +0700

    menambahkan project untuk materi lazy layout pada compose

---
+++ 4_LazyLayoutPadaCompose/JetHeroes/app/src/main/java/com/learning/jetheroes/data/HeroRepository.kt
@@ -0,0 +1,16 @@
+package com.learning.jetheroes.data
+
+import com.learning.jetheroes.model.Hero
+import com.learning.jetheroes.model.HeroesData
+
+class HeroRepository {
+    fun getHeroes(): List<Hero> {
+        return HeroesData.heroes
+    }
+
+    fun searchHeroes(query: String): List<Hero>{
+        return HeroesData.heroes.filter {
+            it.name.contains(query, ignoreCase = true)
+        }
+    }
+}
---
+++ 4_LazyLayoutPadaCompose/JetHeroes/app/src/main/java/com/learning/jetheroes/model/Hero.kt
@@ -0,0 +1,7 @@
+package com.learning.jetheroes.model
+
+data class Hero(
+    val id: String,
+    val name: String,
+    val photoUrl: String
+)
