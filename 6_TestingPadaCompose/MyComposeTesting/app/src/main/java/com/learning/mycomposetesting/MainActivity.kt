27d4ff2f6df78d5646cd861fd0dcf7b40c17f96a
Evan <stevanuspangau50@gmail.com>
2024-09-19 22:38:15 +0700

    materi testing (not all)

---
+++ 6_TestingPadaCompose/MyComposeTesting/app/src/main/java/com/learning/mycomposetesting/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.learning.mycomposetesting
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.learning.mycomposetesting.ui.theme.MyComposeTestingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyComposeTestingTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CalculatorApp()
+                }
+            }
+        }
+    }
+}
